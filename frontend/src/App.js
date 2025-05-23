f38d692721849635950205400b9b77c9864379ca
Chanyoung <33cksdud@gmail.com>
2025-05-23 15:16:34 +0900

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          FrontEnd
+          FrontEnd - chan?
         </a>
       </header>
     </div>
