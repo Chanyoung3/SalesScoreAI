8a248100da9bfef95b358406e90f8ee045c981c4
Chanyoung <33cksdud@gmail.com>
2025-05-28 01:19:22 +0900

    Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Container from "@mui/material/Container";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Login from './components/Login';
+import Consultations from './components/consultations';
+
+const queryClient = new QueryClient();
 
 function App() {
+  const isAuthenticated = !!sessionStorage.getItem("jwt");
+
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Container maxWidth="xl">
+      <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={isAuthenticated ? <Navigate to="/consultations" /> : <Login />}
+            />
+            <Route
+              path="/consultations"
+              element={isAuthenticated ? <Consultations /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Container>
   );
 }
 
